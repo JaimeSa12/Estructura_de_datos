97eb08412263c50da134fbbbf9671b6964b081a5
JaimeSa12 <langsamlernen@gmail.com>
2020-07-07 12:35:39 -0500

    Estructura de datos

--- Lista Circular Doble/listaCD.h
+++ Lista Circular Doble/listaCD.h
@@ -4,18 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct nodoCD{
-	int dato;
-	struct nodoCD *siguiente;
-	struct nodoCD *anterior;
+struct nodoCD
+{
+  int dato;
+  struct nodoCD *siguiente;
+  struct nodoCD *anterior;
 };
 
-struct nodoCD *crear(int);
-void insertar(int);
-int validarPos(int);
-void eliminarPos(int);
-int validarDato(int);
-void eliminarDato(int);
-void mostrar();
+struct nodoCD *crear (int);
+void insertar (int);
+int validarPos (int);
+void eliminarPos (int);
+int validarDato (int);
+void eliminarDato (int);
+void mostrar ();
 
 #endif

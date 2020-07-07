1ec398b7c851eb926f30cdde9e6c12174a8adfa5
JaimeSa12 <langsamlernen@gmail.com>
2020-07-07 12:28:58 -0500

    Estructura de datos

--- Cola Circular/colaC.h
+++ Cola Circular/colaC.h
@@ -6,17 +6,18 @@
 
 #define MAX 3
 
-struct colaC{
-	int dato[MAX];
-	int inicio;
-	int final;
+struct colaC
+{
+  int dato[MAX];
+  int inicio;
+  int final;
 };
 
-int cuantosHay(struct colaC *);
-int colaVacia(struct colaC *);
-int colaLlena(struct colaC *);
-void insertar(struct colaC *, int);
-void eliminar(struct colaC *);
-void mostrar(struct colaC *);
+int cuantosHay (struct colaC *);
+int colaVacia (struct colaC *);
+int colaLlena (struct colaC *);
+void insertar (struct colaC *, int);
+void eliminar (struct colaC *);
+void mostrar (struct colaC *);
 
 #endif
